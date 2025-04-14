1702e29c66981b70be211a96ced683ec4575d795
SUYASH KHARE <suyashkhare1403@gmail.com>
2025-04-14 09:35:26 +0530

    Add files via upload

---
+++ include/config.hpp
@@ -0,0 +1,10 @@
+#include <string>
+const std::string CLIENT_ID = "khGedTfe";
+const std::string CLIENT_SECRET = "0weEB1tYmLiAA1LLw_16redLFxVT6-6sDYP93BfwH28";
+
+const std::string DERIBIT_API_URL = "https://test.deribit.com/api/v2/";
+const std::string WEBSOCKET_URL = "wss://test.deribit.com/ws/api/v2/";
+
+// For live trading, use:
+// const std::string DERIBIT_API_URL = "https://www.deribit.com/api/v2/";
+// const std::string WEBSOCKET_URL = "wss://www.deribit.com/ws/api/v2/";
